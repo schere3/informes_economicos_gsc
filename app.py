84213d2673328b2587fabbd01fb005da53246186
Scherezade <scheretrabajo@gmail.com>
2026-04-20 12:18:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,40 @@ def pagina_principal():
     st.write("Estamos antes la página principal de nuestro estudio")
     st.write("Usa el menú de la izquierda para navegar entre las páginas.")
 
+def visualizar_datos():
+    st.title("Visualización de Datos")
+    st.write("Carga un archivo CSV para visualizar los datos.")
+    archivo_cargado = st.file_uploader("Elige un archivo CSV", type="csv")
+
+    if archivo_cargado is not None:
+        df = pd.read_csv(archivo_cargado)
+        st.write("Datos del archivo csv:")
+        st.write(df)
+        st.write("Estadísticas descriptivas")
+        st.write(df.describe())
+
+def graficos_interactivos():
+    st.title("Gráficos Interactivos")
+    st.write("Carga un archivo CSV para crear gráficos interactivos.")
+    archivo_cargado = st.file_uploader("Elige un archivo CSV", type="csv", key="2")
+    
+    if archivo_cargado is not None:
+        df = pd.read_csv(archivo_cargado)
+        st.write("Elige una columna para el eje X:")
+        eje_x = st.selectbox("Eje X", df.columns)
+        st.write("Elige una columna para el eje Y:")
+        eje_y = st.selectbox("Eje Y", df.columns)
+        if st.button("Crear Gráfico"):
+            fig = px.bar(df, x=eje_x, y=eje_y, title=f"{eje_y} por {eje_x}")
+            st.plotly_chart(fig)
+
 st.sidebar.title("Navegación")
+pagina = st.sidebar.selectbox("Selecciona una página", ["Página Principal", 
+                                                        "Visualización de Datos",
+                                                        "Gráficos Interactivos"])
+if pagina == "Página Principal":
+    pagina_principal()
+elif pagina == "Visualización de Datos":
+    visualizar_datos():
+elif pagina == "Gráficos Interactivos":
+    graficos_interactivos()
