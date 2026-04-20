ef82f703fe0567b1845a3f391a8e4e8805648e56
Scherezade <scheretrabajo@gmail.com>
2026-04-20 12:21:44 +0000

    ultima

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ import plotly.express as px
 pg_intro = st.Page("introduccion.py", title="Introduccion")
 
 # Paginas EDA
-pg_eda_intro = st.Page("seccion_eda/intro_eda.py", title="Primeros Pasos")
-pg_eda_basica = st.Page("seccion_eda/estadisticos_basicos.py", title="Segundo pasos")
+pg1 = st.Page("seccion/pg1.py", title="Primeros Pasos")
+pg2 = st.Page("seccion/pg2.py", title="Segundo pasos")
 
+navigation_env = st.navigation(
+    {
+        "": [pg_intro],
+        "seccion" : [pg1,pg2]
+    }
+)
 
 navigation_env.run()
