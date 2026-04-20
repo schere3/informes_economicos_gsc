69ee247b70c1a19726dc2e43c962aaab1013bd7f
Scherezade <scheretrabajo@gmail.com>
2026-04-20 10:35:27 +0000

    commit3

---
+++ app.py
@@ -0,0 +1,10 @@
+import stramlit as st
+import pandas as pd
+import plotly.express as px
+
+def pagina_principal():
+    st.title("Página Principal")
+    st.write("Estamos antes la página principal de nuestro estudio")
+    st.write("Usa el menú de la izquierda para navegar entre las páginas.")
+
+st.sidebar.title("Navegación")
