2c430eaf00cff78c3625f50db944fb6062d45c83
Scherezade <scheretrabajo@gmail.com>
2026-04-20 18:34:00 +0000

    comite

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import plotly.express as px
 pg_intro = st.Page("introduccion.py", title="Informes Económicos")   # Introducción
 pg1 = st.Page("pred_electricidad.py", title="Predicción Gastos de Electricidad 2026")  # gastos electricidad
 # seccion gastos electricidad
-pg_1 = st.Page("seccion/pg_1.py", title="Primeros Pasos")
-pg_2 = st.Page("seccion/pg_2.py", title="Segundo pasos")
+pg_1 = st.Page("seccion/pg1.py", title="Primeros Pasos")
+pg_2 = st.Page("seccion/pg2.py", title="Segundo pasos")
 pg2 = st.Page("materialesfungibles.py", title="Gestión de materiales fungibles")       # materiales fungibles
 pg3 = st.Page("maquinas_vending.py", title="Máquinas Vending")                         # maquinas vending
 pg4 = st.Page("suministro_agua.py", title="Concurso de Suministro de Agua")            # suministro de agua
