48e5b0fa632f0621819f852d960c3e1b45062d52
Scherezade <scheretrabajo@gmail.com>
2026-04-20 18:06:14 +0000

    commit

--- app.py
+++ app.py
@@ -2,17 +2,26 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-pg_intro = st.Page("introduccion.py", title="Introduccion")
-
-# Paginas EDA
-pg1 = st.Page("seccion/pg1.py", title="Primeros Pasos")
-pg2 = st.Page("seccion/pg2.py", title="Segundo pasos")
+# Asignación de páginas
+pg_intro = st.Page("introduccion.py", title="Informes Económicos")   # Introducción
+pg1 = st.Page("pred_electricidad.py", title="Predicción Gastos de Electricidad 2026")  # gastos electricidad
+# seccion gastos electricidad
+pg_1 = st.Page("seccion/pg1.py", title="Primeros Pasos")
+pg_2 = st.Page("seccion/pg2.py", title="Segundo pasos")
+pg2 = st.Page("materialesfungibles.py", title="Gestión de materiales fungibles")       # materiales fungibles
+pg3 = st.Page("maquinas_vending.py", title="Máquinas Vending")                         # maquinas vending
+pg4 = st.Page("suministro_agua.py", title="Concurso de Suministro de Agua")            # suministro de agua
+pg5 = st.Page("datos_sinteticos.py", title="Generación de Datos Sintéticos")           # datos sinteticos
 
 navigation_env = st.navigation(
     {
         "": [pg_intro],
-        "seccion" : [pg1,pg2]
+        "Gastos de electricidad 2026" : [pg1],
+        "Sección": [pg_1,pg_2],
+        "Gestión de materiales fungibles": [pag2],
+        "Máquinas Vending": [pag3],
+        "Concurso de Suministro de Agua": [pag4],
+        "Generación de Datos Sintéticos": [pag5]
     }
 )
-
 navigation_env.run()
--- introduccion.py
+++ introduccion.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.title("Bienvenidos a la int5roduccion de este informe")
-st.write("Este informe va cogiendo forma poco a poco")
+st.title("Informes económicos")
+st.write("Se abordan estudios e informes económicos para la formalización de contratos de cualquier tipo de compras o prestación de servicios, entre otros aspectos.")
+st.write(" Los temas a tratar:" \
+" * Predicción Gastos de Electricidad 2026 " \
+" * Gestión de Materiales Fungibles" \
+" * Máquinas Vending" \
+" * Concurso de Suministro de Agua" \
+" * Generación de Datos Sintéticos")
